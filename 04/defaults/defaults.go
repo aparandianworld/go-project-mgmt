e5021a912c23f61ca73be1f0edae63f45ae8fb0f
Aaron Parandian <aparandian@apple.com>
2025-12-02 11:54:22 -0800

    04. init function and multiple init function ordering

---
+++ 04/defaults/defaults.go
@@ -0,0 +1,31 @@
+package defaults
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Default variables here
+var (
+	ItemPrice    = 100
+	ItemDiscount = 10
+)
+
+func BlackFridayDiscount() int {
+	return ItemDiscount * 2
+}
+
+func randomDiscount() int {
+	min := 1
+	max := 50
+	ItemDiscount = rand.Intn(max-min+1) + min
+	return ItemDiscount
+}
+
+func RandomDiscount() int {
+	return randomDiscount()
+}
+
+func init() {
+	fmt.Println("Init() in defaults.go called...")
+}
